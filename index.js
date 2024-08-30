9388703627c6bce971afdc76d27e283786a39d8c
BurcuMengu <burcumengu@gmail.com>
2024-08-30 22:32:10 +0300

    push

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 import passport from "passport";
 import { Strategy } from "passport-local";
 import session from "express-session";
+import GoogleStrategy from "passport-google-oauth2";
 import env from "dotenv";
 
 const app = express();
@@ -136,6 +137,17 @@ passport.use(
     } catch (err) {
       console.log(err);
     }
+  })
+);
+
+passport.use("google", 
+  new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "http://www.googleapis.com/oauth2/v3/userinfo",
+  }, async (accessToken, refreshToken, profile, cb) => {
+    
   })
 );
 
